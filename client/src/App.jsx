33724a9b80428a7815199de5bbceedca471efacd
TepChoulong <tepchoulongzin@gmail.com>
2024-01-20 21:15:25 +0700

    Made a responsive header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import "./styles/app.css";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import "./styles/app.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/*<Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import "../styles/components/header.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default function header() {
+  return (
+    <header>
+      <div className="logo">
+        <img
+          src="https://i1.sndcdn.com/artworks-000054056037-kaol0s-t500x500.jpg"
+          alt="This is logo"
+        />
+        <h1>MyBlogNews</h1>
+      </div>
+      <div className="links-container">
+        {/* Desktop */}
+        <div className="display-links-container-desktop">
+          <ul>
+            <li>
+              <Link className="link">Home</Link>
+            </li>
+            <li>
+              <Link className="link">Sports</Link>
+            </li>
+            <li>
+              <Link className="link">Technologies</Link>
+            </li>
+            <li>
+              <Link className="link">Business</Link>
+            </li>
+          </ul>
+        </div>
+        {/* Mobile */}
+        <div className="display-links-container-mobile">
+          <button
+            onClick={() => {
+              var contents = document.getElementById("myDropdown-contents");
+              if (contents.style.display === "none") {
+                contents.style.display = "block";
+              } else {
+                contents.style.display = "none";
+              }
+            }}
+            class="dropdown-btn"
+          >
+            <i class="bi bi-list"></i>
+          </button>
+          <div
+            className="dropdown-content"
+            id="myDropdown-contents"
+            style={{ display: "none" }}
+          >
+            <ul>
+              <li>
+                <Link className="link">Home</Link>
+              </li>
+              <li>
+                <Link className="link">Sports</Link>
+              </li>
+              <li>
+                <Link className="link">Technologies</Link>
+              </li>
+              <li>
+                <Link className="link">Business</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
