9a5f4c27ccf18552b9808aedaee75a84cb3461e1
TepChoulong <tepchoulongzin@gmail.com>
2024-01-21 11:38:31 +0700

    Update more in homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import Trending_Post from "../components/Trending_Post";
 import Breaking_News_Post from "../components/Breaking_News_Post";
+import Breaking_News_Choices from "../components/Breaking_New_Choices";
 
 export default function Home() {
   return (
@@ -12,15 +13,15 @@ export default function Home() {
         <div className="post-contents-container">
           {/* Post-1 */}
           <Trending_Post />
-          {/* Post-2 */}
-          <Trending_Post />
         </div>
       </div>
       <div className="breaking_news-container">
         <h1>Breaking News</h1>
+        <div className="breaking_news-choices-container">
+          <Breaking_News_Choices />
+        </div>
         <div className="breaking_news-post-contents-container">
           {/* Breaking News Post 1 */}
-          <Breaking_News_Post />
           <Breaking_News_Post />
           <Breaking_News_Post />
           <Breaking_News_Post />
