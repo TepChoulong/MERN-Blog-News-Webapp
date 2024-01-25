146f8b5191d45af3edbe11bc007cbc66484b7cf8
VannyTep <89964001+VannyTep@users.noreply.github.com>
2024-01-25 21:45:10 +0700

    i have write the html form of registerpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import "./styles/app.css";
+
 import Layout from "./Layout";
 import Home from "./pages/Home";
-import "./styles/app.css";
+import LoginPage from "./pages/Loginpage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
           {/*<Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
+          \
         </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
