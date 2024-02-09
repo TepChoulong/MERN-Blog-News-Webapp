384cad46c6aa518388d57616254fc04b505a141c
VannyTep <89964001+VannyTep@users.noreply.github.com>
2024-02-09 14:36:22 +0700

    Fix confilct

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,20 @@ app.use(cookieParser());
 
 /* !Database Connection */
 mongoose // MongoDB Atlas Password: ls4daG4FWdVsCGlq
-  .connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database connected!"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connected!"))
+    .catch((err) => console.log(err));
 
-<<<<<<< HEAD
 // MongoDB Atlas Password: ls4daG4FWdVsCGlq
-=======
+
 app.use("/api", require("./routes/authRoute"));
 app.use("/profile", require("./routes/profileRoute"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
->>>>>>> a814be23f37a580689c387aff7c4232795d666a8
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
